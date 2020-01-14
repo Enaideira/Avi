7e3f985ef95ed47d2bb92ef66c47ab29dfb52dd4
Sora Itsuro <mitch817@gmail.com>
2020-01-14 12:07:21 +0000

    Removed Space
    as above

--- avibot/index.js
+++ avibot/index.js
@@ -1,6 +1,5 @@
 let config = require("./config").config;
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { token: config.DISCORD_API_TOKEN });
-
 manager.spawn();
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
